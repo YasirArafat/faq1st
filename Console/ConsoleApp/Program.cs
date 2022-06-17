be9810cb07daa7497e52df9eb98d62beee8fa960
unknown <arafatyasir56@yahoo.com>
2022-06-17 22:02:53 +0600

    Data Post Issue: 1 hour, Sql: 1 hour, Report: 2.5 hour

---
+++ Console/ConsoleApp/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public static List<int> GetRank(List<int> ranked, List<int> player)
+        {
+            var result = new List<int>();
+
+            ranked = ranked.OrderByDescending(i => i).ToList();
+            player.Sort();
+            Dictionary<int, int> rankedIndexes = new Dictionary<int, int>();
+            int k = 1;
+            for (int i = 0; i < ranked.Count; i++)
+            {
+                if (!rankedIndexes.ContainsValue(ranked[i]))
+                {
+                    rankedIndexes.Add(k, ranked[i]);
+                    k++;
+                }
+            }
+            int j = 0;
+            foreach (int p in player)
+            {
+                result.Add(j);
+                //rankedIndexes.Where(r=> r.Value >= p && r.Value <p)
+                foreach (KeyValuePair<int, int> rankedIndex in rankedIndexes)
+                {
+                    if (p >= ranked.Max())
+                    {
+                        result[j] = 1;
+                    }
+                    else
+                    {
+                        if (!rankedIndexes.ContainsValue(p))
+                        {
+                            if (rankedIndex.Value < p)
+                            {
+                                result[j] = rankedIndex.Key == 1 ? 1 : rankedIndex.Key - 1;
+                            }
+                            else if (rankedIndex.Value > p)
+                            {
+                                result[j] = rankedIndex.Key + 1;
+                            }
+                            else
+                            {
+                                result[j] = rankedIndex.Key;
+                            }
+                        }
+                    }
+                }
+                j++;
+            }
+
+            return result;
+        }
+        static void Main(string[] args)
+        {
+            //TODO: Change this input file path before start
+            var filePath = "D:\\FaqInterview\\InterviewSourceCode\\Console\\ConsoleApp\\Input.txt";
+
+            var lines = System.IO.File.ReadLines(filePath).ToList();
+
+            int rankedCount = Convert.ToInt32(lines[0].Trim());
+
+            List<int> ranked = lines[1].TrimEnd().Split(' ').ToList().Select(rankedTemp => Convert.ToInt32(rankedTemp)).ToList();
+
+            int playerCount = Convert.ToInt32(lines[2].Trim());
+
+            List<int> player = lines[3].TrimEnd().Split(' ').ToList().Select(playerTemp => Convert.ToInt32(playerTemp)).ToList();
+
+            List<int> result = GetRank(ranked, player);
+
+            Console.WriteLine(String.Join("\n", result));
+
+        }
+    }
+}
---
+++ Faq.Interview/Models/InterviewViewModels.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web.Mvc;
+
+public class DataListItem
+{
+    public string Value { get; set; }
+    public string Text { get; set; }
+}
+
+public class AutocompleteListItem
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+
+public class ValidationViewModel
+{
+
+    public string Name { get; set; }
+    public DateTime Date { get; set; }
+    public bool Agreed { get; set; }
+}
+
+public class DinnerViewModel
+{
+    public DinnerViewModel()
+    {
+        Sizes = new List<Size>();
+    }
+    public string Name { get; set; }
+    [AllowHtml]
+    public string Description { get; set; }
+    public List<Size> Sizes { get; set; }
+}
+
+public class Size
+{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public double Unit { get; set; }
+}
+
+public class Employee
+{
+    public Employee()
+    {
+        Salaries = new List<Salary>();
+        WeeklySalary = new WeeklySalary();
+    }
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public List<Salary> Salaries { get; set; }
+    public WeeklySalary WeeklySalary { get; set; }
+}
+
+public class Salary
+{
+    public int EmployeeId { get; set; }
+    public DateTime Date { get; set; }
+    public double Amount { get; set; }
+}
+
+public class WeeklySalary
+{
+    public double Week1Salary { get; set; }
+    public double Week2Salary { get; set; }
+    public double Week3Salary { get; set; }
+    public double Week4Salary { get; set; }
+    public double Week5Salary { get; set; }
+}
